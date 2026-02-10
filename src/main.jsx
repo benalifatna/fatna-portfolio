adeb9fe406cd0a243019c7936b641ee5453cae78
Fatna <benalifatna02@gmail.com>
2026-02-10 13:09:45 +0100

    retrait du script module dans l'index.html

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,20 @@ import './index.css'
 import App from './App.jsx'
 // import reportWebVitals from './reportWebVitals';
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
